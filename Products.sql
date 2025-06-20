e4e3a842ba3a50b014fa72d5b50029e2a784f993
Subhransunayak17 <80567403+Subhransunayak17@users.noreply.github.com>
2025-06-20 12:57:53 +0530

    Add files via upload

---
+++ Products.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Products (
+    ProductID INT, 
+    Sub_Category_Key VARCHAR(50), 
+    Color VARCHAR(50), 
+    ProductName VARCHAR(50), 
+    RetailPrice VARCHAR(50), 
+    StandardCost VARCHAR(50)
+);
+select * from products;
